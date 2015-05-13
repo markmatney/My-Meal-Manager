696b687959ff46fde770bab6d644b96f9c564ea9
markmatney <markamatney@gmail.com>
2015-05-13 02:36:03 -0700

    Refactor parser code to fit template design pattern

--- webscraper/RecipeSQLGen.java
+++ webscraper/RecipeSQLGen.java
@@ -38,24 +38,7 @@ class Main
     int i, n, listsize = 0, garbageflag = 0;
     char c;
 
-/*
-    //HTTP GET test
-    String url = "https://www.google.com/search?num=1&as_q=grilled+cheese&as_sitesearch=allrecipes.com";
-
-    Document doc = Jsoup.connect(url).userAgent("Mozilla/5.0")
-			.timeout(5000).get(); // Java representation of DOM that our class methods will parse
-    Elements links = doc.select("h3.r > a");
-    for (Element link : links)
-    {
-        // Extract innner html from elements containing ingredient name and amount
-       doc = Jsoup.connect(link.attr("abs:href")).get();
-        System.out.println(doc.location());
-    }
-*/
-
-
-    // TODO: Write query to create database and tables
-    String query = "CREATE TABLE something something mysql\n", subquery, recipe = "", recipeurl;
+    String query = "", subquery, recipe = "";
     ArrayList<String> recipes = new ArrayList<String>();    
     FileInputStream f;
 
@@ -101,69 +84,34 @@ class Main
     {
       recipe = it.next();
 
-      // For each recipe, use Jsoup to do a Google search, then grab URL of recipe page.
-      String recipeName = recipe.replaceAll(" ", "%20").replaceAll("&", "%26").replaceAll("\n", "");
-
-      System.out.println(recipeName);
-
-      //String url = "https://www.google.com/search?num=1&as_sitesearch=allrecipes.com&as_q=";
-      String url = "http://allrecipes.com/search/default.aspx?wt=";
-      url = url.concat(recipeName);
-
-      //REMOVE LATER
-      System.out.println("Allrecipes search URL: " + url);
-
-      Document doc = Jsoup.connect(url).get(); // Java representation of DOM that our class methods will parse
-      Elements links = doc.select("a.title");
-      recipeurl = "";
-      for (Element link : links)
-      {
-        // Extract innner html from elements containing ingredient name and amount
-        //doc = Jsoup.connect(link.attr("abs:href")).get();
-        //recipeurl = doc.location();
-        recipeurl = link.attr("abs:href");
-      }
-
-      //REMOVE LATER
-      System.out.println("First result allrecipes URL: " + recipeurl + "\n");
-
-      //recipeurl = "http://allrecipes.com/Recipe/Risotto-alla-Milanese/";
-      // For example; replace with appropriate implementation of search/URL scrape.
-
-
-      Recipe r = new Recipe(recipeurl);
-
-      System.out.println(r.toString());
-
-      // TODO: Iterate over this data structure and construct an SQL subquery (sql-ify)
-      subquery = "INSERT into table blah blah blah" + r.url;
-
-      // Append subquery to query.
-      query += subquery + "\n";
+      AllRecipesRecipe arr = new AllRecipesRecipe(recipe);
+      subquery = arr.generateQuery();
+      query += subquery + "\n\n";
     }
-    //System.out.println(query);
+
+    System.out.println("** in main: \n" + query);
   }
 }
 
- 
-
-class Recipe
+abstract class Recipe
 {
-  // source url, list of ingredients, instructions
-  public String url;
-  public ArrayList<Ingredient> ingredients;
-  public ArrayList<String> instructions;
+  // members
+  protected String name;
+  protected String url;
+  // image??
+  //
+  protected Document dom;
+  protected String time;
+  protected ArrayList<Ingredient> ingredients;
+  protected ArrayList<String> instructions;
 
-  // constructor
-  public Recipe(String src) throws IOException
+  // functions
+
+  public Recipe(String n)
   {
-    url = src;
-    Document doc = Jsoup.connect(src).get(); // Java representation of DOM that our class methods will parse
-    ingredients = getIngredients(src, doc);
-    instructions = getInstructions(src, doc);
+    name = n;
   }
 
-  // for debugging purposes
   public String toString()
   {  
     String ret = "Recipe:\n\turl: " + url + "\n\tingreds: " + ingredients.toString() + "\n\tinsns: \n";
@@ -177,16 +125,68 @@ class Recipe
     return ret;
   }
 
-  /* getIngredients
-   *
-   * Input:
-   *   src - source url to scrape ingredients from
-   *   doc - jsoup object representing the DOM of src
-   *
-   * Output:
-   *   ArrayList of Ingredient objects, each containing name, quantity, and units
-   */
-  public ArrayList<Ingredient> getIngredients(String src, Document doc)
+  final String generateQuery() throws IOException, Exception
+  {
+    try {
+      createDOM();
+      parseDOM();
+      return convertDataToQuery();
+    }
+    catch (Exception e) { // cannot generate recipe for some reason (no search results, etc.)
+      return "";
+    }
+  }
+
+  abstract void createDOM() throws IOException;
+  abstract void getIngredients();
+  abstract void getInstructions();
+  abstract void getTime();
+
+  final void parseDOM()
+  {
+    getIngredients();
+    getInstructions();
+    getTime();
+  }
+
+  final String convertDataToQuery()
+  {
+    // fucked up part
+    String id = "6", time = "40";
+
+    // not sure why java is concatenating these incorrectly
+    System.out.println("** in method: Recipe.convertDataToQuery() **");
+    return "INSERT INTO Recipes VALUES (" + id + "," + name + ",'" + url + "')";
+  }
+}
+
+class AllRecipesRecipe extends Recipe
+{
+  public AllRecipesRecipe(String n)
+  {
+    super(n);
+  }
+
+  public void createDOM() throws IOException
+  {  
+    String searchurl = "http://allrecipes.com/search/default.aspx?wt=" + name.replaceAll(" ", "%20").replaceAll("&", "%26").replaceAll("\n", "");
+
+    Document doc1 = Jsoup.connect(searchurl).get(); // Java representation of DOM that our class methods will parse
+    Elements links = doc1.select("a.title");
+    String recipeurl = "";
+    for (Element link : links)
+    {
+      // Extract innner html from elements containing ingredient name and amount
+      //doc = Jsoup.connect(link.attr("abs:href")).get();
+      //recipeurl = doc.location();
+      recipeurl = link.attr("abs:href");
+    }
+    url = recipeurl; 
+    System.out.println(url);
+    dom = Jsoup.connect(recipeurl).get(); // Java representation of DOM that our class methods will parse
+  }
+
+  public void getIngredients()
   {
     Ingredient i;
 
@@ -196,110 +196,76 @@ class Recipe
     String n, q, u, amt, site = "some regex";
     ArrayList<Ingredient> is = new ArrayList<Ingredient>();
 
-    site = "allrecipes"; // delete after regex works
-    
-    if ("allrecipes" == site)
-    {
       // In the allrecipes recipe pages, the ingredients are located inside <p class="fl-ing">
-      Elements ps = doc.select("p.fl-ing");
-      for (Element p : ps)
+    Elements ps = dom.select("p.fl-ing");
+    for (Element p : ps)
+    {
+      // Extract innner html from elements containing ingredient name and amount
+      n = p.select(".ingredient-name").html();
+      // TODO: May want to further process name?
+      amt = p.select(".ingredient-amount").html();
+
+      // Split amount into quantity (numerical) and units (alpabetical)
+      // All characters that are not letters (i.e., digits, spaces, and /) belong to quantity
+      // e.g. "1 1/4"
+      int j;
+      for (j = 0; j < amt.length() && !Character.isLetter(amt.charAt(j)); j++) {}
+ 
+      // TODO: if there is something in parenthesis, that is the units! FIX
+
+      // TODO: does this ever happen when amt.length() != 0 ?
+      if (j == 0) // e.g. "salt and pepper to taste"
       {
-        // Extract innner html from elements containing ingredient name and amount
-        n = p.select(".ingredient-name").html();
-        // TODO: May want to further process name?
-
-        amt = p.select(".ingredient-amount").html();
-
-        // Split amount into quantity (numerical) and units (alpabetical)
-        // All characters that are not letters (i.e., digits, spaces, and /) belong to quantity
-        // e.g. "1 1/4"
-        int j;
-        for (j = 0; j < amt.length() && !Character.isLetter(amt.charAt(j)); j++) {}
-
-        // TODO: if there is something in parenthesis, that is the units! FIX
-
-        // TODO: does this ever happen when amt.length() != 0 ?
-        if (j == 0) // e.g. "salt and pepper to taste"
+        q = ""; // should do something better than this eventually
+        u = "";
+      }
+      else
+      {
+        if (j != amt.length()) // there are units
         {
-          q = ""; // should do something better than this eventually
+          j--;  
+          q = amt.substring(0, j);
+ 
+          // Remaining characters represent belong to units, e.g. "tablespoon"
+          u = amt.substring(j + 1);
+
+          // singularize units (remove 's' from the end)
+          if (u.length() > 0 && u.charAt(u.length() - 1) == 's')
+          {
+            u = u.substring(0, u.length() - 1);
+          }
+        }
+        else // no units
+        {
+          q = amt;
           u = "";
         }
-        else
-        {
-          if (j != amt.length()) // there are units
-          {
-            j--;  
-            q = amt.substring(0, j);
-
-            // Remaining characters represent belong to units, e.g. "tablespoon"
-            u = amt.substring(j + 1);
-
-            // singularize units (remove 's' from the end)
-            if (u.length() > 0 && u.charAt(u.length() - 1) == 's')
-            {
-              u = u.substring(0, u.length() - 1);
-            }
-          }
-          else // no units
-          {
-            q = amt;
-            u = "";
-          }
-        }
-
-        i = new Ingredient(n, q, u);
-        is.add(i);
       }
+      i = new Ingredient(n, q, u);
+      is.add(i);
     }
-    // TODO: Enable scraping from other sites as well. Study source HTML and figure out how to parse.
-    else if ("simplyrecipes" == site)
-    {
-
-    }
-    else if ("tasteofhome" == site)
-    {
-
-    }
-    else
-    {
-      // TODO: Decide what to do for this case.
-      System.out.println("unknown site");
-    }
-    return is;
+    ingredients = is;
   }
 
-  public ArrayList<String> getInstructions(String src, Document doc)
+  public void getInstructions()
   {
+    // get instructions
     ArrayList<String> insns = new ArrayList<String>();
-    String insn, site = "regex on src";
-    site = "allrecipes";
+    String insn;
 
-    if ("allrecipes" == site)
+    // Instructions are inside an <ol> immediately following <div id="msgDirections">
+    Elements ol = dom.select("div#msgDirections + ol");
+    Elements lis = ol.select("li");
+    for (Element li : lis)
     {
-      // Instructions are inside an <ol> immediately following <div id="msgDirections">
-      Elements ol = doc.select("div#msgDirections + ol");
-      Elements lis = ol.select("li");
-      for (Element li : lis)
-      {
-        insns.add(li.select("span").html());;
-      }
-      
+      insns.add(li.select("span").html());;
     }
-    // TODO: Enable scraping from other sites.
-    else if ("simplyrecipes" == site)
-    {
+    instructions = insns;
+  }
 
-    }
-    else if ("tasteofhome" == site)
-    {
-
-    }
-    else
-    {
-      // TODO: Decide what to do for this case.
-      System.out.println("unknown site");
-    }
-    return insns;
+  public void getTime()
+  {
+    // set time to the total cook time (in minutes)
   }
 }
 
